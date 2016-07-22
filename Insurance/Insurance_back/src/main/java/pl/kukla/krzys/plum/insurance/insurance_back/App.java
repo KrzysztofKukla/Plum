0a611dfbb81e27bc831e7ce5b3f65b760da61c18
KrzysztofKukla <krzys.kukla@gmail.com>
2016-07-22 22:24:51 +0200

    First commit

---
+++ Insurance/Insurance_back/src/main/java/pl/kukla/krzys/plum/insurance/insurance_back/App.java
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.plum.insurance.insurance_back;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Mobile/Mobile_back/src/main/java/pl/kukla/krzys/plum/mobile/mobile_back/App.java
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.plum.mobile.mobile_back;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
